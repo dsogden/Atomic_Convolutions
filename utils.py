21f35735f893c81776c12eac83b940e3932f8aa3
Dylan Ogden <67968474+dsogden@users.noreply.github.com>
2024-01-31 10:06:22 -0600

    Update and rename load_data.py to utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 def load_data(path, names):
     X_1, Nbrs_1, Nbrs_Z_1 = np.load('%s.npy' % (path + names[0])), np.load('%s.npy' % (path + names[1])), np.load('%s.npy' % (path + names[2]))
@@ -7,3 +8,18 @@ def load_data(path, names):
     labels = np.load('%s/labels.npy' % (path))
 
     return X_1, Nbrs_1, Nbrs_Z_1, X_2, Nbrs_2, Nbrs_Z_2, X_complex, Nbrs_complex, Nbrs_Z_complex, labels
+
+def train_step(inputs, y, model, loss, optimizer):
+    outputs = model(inputs)
+    loss = criterion(outputs, y)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    return loss.item()
+
+def eval_step(inputs, y, model):
+    outputs = model(inputs)
+    loss = criterion(outputs, y)
+
+    return loss.item()
